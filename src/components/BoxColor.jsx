e842dbe4e1af562accac5b2bbe23b3ca27cc80ad
Adrian <adrianginergimenez@gmail.com>
2024-07-15 11:12:11 +0200

    feat: improved box text

--- src/components/BoxColor.jsx
+++ src/components/BoxColor.jsx
@@ -1,7 +1,7 @@
 function BoxColor ({color, value}) {
   return (
     <div className={["box", color, color === value ? "full" : ""].join(' ')}>
-      <p>{color === value ? `Yes, I'm color ${value ? value : ""}` : `Im not a color ${value ? value : ""}`}</p>
+      <p>{color === value ? `Yes, I'm a color ${value ? value : ""} box!` : `Im not a color ${value ? value : ""} box!`}</p>
     </div>
   )
 }
